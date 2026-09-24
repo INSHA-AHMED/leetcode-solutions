2a8dda48382ee685cb4740086417ca5a79885adc
INSHA-AHMED <inshaahmed2025@gmail.com>
2026-09-24 12:38:27 +0530

    Implement valid parentheses checker in C

---
+++ basic-algorithms/08-valid-parentheses.c
@@ -0,0 +1,28 @@
+#include <stdbool.h>
+
+bool isValid(char* s) {
+    char stack[10000];
+    int top = -1;
+
+    for (int i = 0; s[i] != '\0'; i++) {
+        char c = s[i];
+
+        if (c == '(' || c == '[' || c == '{') {
+            stack[++top] = c;
+        } else {
+            if (top < 0) {
+                return false;
+            }
+
+            char open = stack[top--];
+
+            if ((c == ')' && open != '(') ||
+                (c == ']' && open != '[') ||
+                (c == '}' && open != '{')) {
+                return false;
+            }
+        }
+    }
+
+    return top == -1;
+}
